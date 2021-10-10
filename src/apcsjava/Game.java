d0b06361a518d1feaebb163533286de4a952229f
DerekDenoon <86374922+DerekDenoon@users.noreply.github.com>
2021-10-09 20:58:24 -0400

    bonus specs

--- src/apcsjava/Game.java
+++ src/apcsjava/Game.java
@@ -1,39 +1,51 @@
 package apcsjava;
 
 public class Game {
-    public static void main(String[] args) {
-        runGame(10,10,1);
+    private int discretePlayersHit;
+    private boolean overHalfHit;
 
-
+    public Game(int targetCount, int throwerCount, double hitProbability) {
+        runGame(targetCount,throwerCount,hitProbability);
+    }
+    public int getDiscretePlayersHit() {
+        return discretePlayersHit;
+    }
+    public void setDiscretePlayersHit(int discretePlayersHit) {
+        this.discretePlayersHit = discretePlayersHit;
+    }
+    public boolean isOverHalfHit() {
+        return overHalfHit;
+    }
+    public void setOverHalfHit(boolean overHalfHit) {
+        this.overHalfHit = overHalfHit;
     }
 
-    public static void runGame(int targetCount,int throwerCount, double hitProbability){
+    public void runGame(int targetCount, int throwerCount, double hitProbability){
 
         Thrower[] throwers = new Thrower[throwerCount];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < throwerCount; i++) {
             throwers[i] = new Thrower();
         }
 
         Target[] targets = new Target[targetCount];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < targetCount; i++) {
             targets[i] = new Target();
         }
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < throwerCount; i++){
             int targetHit = throwers[i].throwBall(targetCount,hitProbability);
             if(targetHit != -1){
                 targets[targetHit].hitCountPlusOne();
             }
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(targets[i].getHitCount());
-        }
 
-        System.out.println("targets hit is " + discretePlayersHit(targets));
+       this.setDiscretePlayersHit(discretePlayersHit(targets));
+
+       this.setOverHalfHit(overHalfHit(this.discretePlayersHit,targetCount));
 
     }
 
-    public static int discretePlayersHit(Target[] in){
+    private static int discretePlayersHit(Target[] in){
         Target[] targets = in.clone();
         int playersHit = 0;
         for (int i = 0; i< targets.length;i++){
@@ -45,7 +57,9 @@ public class Game {
         return playersHit;
     }
 
-    public static void ProbOverHalfHit(Target[] in){
-
+    private static boolean overHalfHit(int playersHit,int totalPlayers){
+        return playersHit > totalPlayers / 2;
     }
+
+
 }
--- src/apcsjava/Simulation.java
+++ src/apcsjava/Simulation.java
@@ -1,5 +1,51 @@
 package apcsjava;
 
+
+import java.util.Scanner;
+
 public class Simulation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int numberOfThrowers;
+        int numberOfTargets;
+        int trials;
+        double hitChance;
+
+        System.out.println("Enter the number of throwers");
+        numberOfThrowers = in.nextInt();
+        System.out.println("Enter the number of targets");
+        numberOfTargets = in.nextInt();
+        System.out.println("Enter the number of trials");
+        trials = in.nextInt();
+        System.out.println("Enter the hit probability (as a decimal)");
+        hitChance = in.nextDouble();
+
+        double avgPlayersHit = 0;
+        double probOverHalf = 0;
+        Game[] games = new Game[trials];
+
+        for (int i = 0; i < trials; i++) {
+            games[i] = new Game(numberOfTargets,numberOfThrowers,hitChance);
+        }
+
+        for (int i = 0; i < trials; i++) {
+            avgPlayersHit += games[i].getDiscretePlayersHit();
+        }
+
+        avgPlayersHit = avgPlayersHit/trials;
+
+        for (int i = 0; i < trials; i++) {
+            if (games[i].isOverHalfHit()){
+                probOverHalf++;
+            }
+        }
+        probOverHalf = probOverHalf/trials;
+
+        System.out.println("avg hit is " + avgPlayersHit);
+        System.out.println("avg over half is " + probOverHalf);
+
+
+    }
 
 }
+
